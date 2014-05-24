8af1c6157d41d6ae5ce49a535dd30fc74d6bdab8
Travis Collins <travis@tec27.com>
2014-05-24 14:57:36 -0700

    Add loading indicator.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,10 @@
 var angular = require('angular')
 
 var app = angular.module('gifselfies', [ ])
+
+app.run(function($rootScope, $timeout) {
+  $timeout(function() {
+    $rootScope.appInitialized = true
+    $rootScope.apply()
+  }, 0)
+})

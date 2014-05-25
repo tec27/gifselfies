1c95bf69f38183d20ee148fe2ac54b63d378235c
Travis Collins <travis@tec27.com>
2014-05-24 17:03:35 -0700

    Implement webcam directive up to the point of grabbing the webcam and displaying errors.

--- client/selfie-taker.js
+++ client/selfie-taker.js
@@ -5,12 +5,60 @@ module.exports = 'gifselfies.selfie-taker'
 var mod = angular.module(module.exports, [])
 
 mod.controller('SelfieTakerCtrl', function($scope) {
+  $scope.$on('gsWebcamError', function(scope, elem, err) {
+    $scope.webcamError = err.name
+  })
+
+  $scope.$on('gsWebcamReady', function(scope, elem, srcWidth, srcHeight) {
+    console.log('webcam is ready!')
+  })
 })
+
+var getUserMedia = navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia
+var url = window.URL || window.webkitURL || window.mozURL || window.msURL
+
+if (getUserMedia) getUserMedia = getUserMedia.bind(navigator)
 
 mod.directive('gsWebcam', function() {
   function link(scope, element, attrs) {
     if (element.prop('tagName') != 'VIDEO') {
       throw new Error('gsWebcam can only be used on Video elements')
+    }
+    if (!getUserMedia) {
+      return
+    }
+
+    // TODO(tec27): make customizable through attributes
+    var width = 640
+      , height = 480
+    getUserMedia({
+      video: {
+        mandatory: {
+          minWidth: width,
+          minHeight: height
+        }
+      }
+    }, success, failure)
+
+    function success(stream) {
+      var elem = element[0]
+      if (elem.mozSrcObject) {
+        elem.mozSrcObject = stream
+      } else {
+        elem.src = url.createObjectURL(stream)
+      }
+
+      elem.play()
+      scope.$emit('gsWebcamReady', element, elem.videoWidth, elem.videoHeight)
+      scope.$apply()
+    }
+
+    function failure(err) {
+      scope.$emit('gsWebcamError', element, err)
+      scope.$apply()
     }
   }
 
